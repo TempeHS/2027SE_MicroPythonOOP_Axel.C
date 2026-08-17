4d3bcd6fa8b0c2912197c5993eacdc4e0147466b
Axel C <159206642+TheSpiciestRamen@users.noreply.github.com>
2026-08-17 00:59:20 +0000

    added polymorphism overrides for high and low and toggle methods

--- project/py_scripts/v03.py
+++ project/py_scripts/v03.py
@@ -3,12 +3,43 @@ from time import sleep
 
 
 class LedLight(Pin):
-    def __init__(self, pin):
+    # Sub Class inherits the 'Pin' Class
+    def __init__(self, pin, flashing=False, debug=False):
         super().__init__(pin, Pin.OUT)
+        self.__debug = debug
+        self.__pin = pin
+        self.__flashing = flashing
+
+    def on(self):
+        # method overriding polymorphism of the Super Class
+        self.high()
+        if self.__debug:
+            print(f"LED connected to Pin {self.__pin} is high")
+
+    def off(self):
+        # method overriding polymorphism of the Super Class
+        self.low()
+        if self.__debug:
+            print(f"LED connected to Pin {self.__pin} is low")
+
+    def toggle(self):
+        # method overriding polymorphism of the Super Class
+        if self.value() == 0:
+            self.high()
+            if self.__debug:
+                print(f"LED connected to Pin {self.__pin} is high")
+        elif self.value() == 1:
+            self.low()
+            if self.__debug:
+                print(f"LED connected to Pin {self.__pin} is low")
 
 
-red_light = LedLight(3)
-
+red_light = LedLight(3, False, True)
+green_light = LedLight(6, False, True)
 while True:
-    red_light.toggle()
-sleep(1)
+    red_light.on()
+    green_light.on()
+    sleep(1)
+    red_light.off()
+    green_light.off()
+    sleep(1)
